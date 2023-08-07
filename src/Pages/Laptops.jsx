f99501a9997c7aa23b956a95af2ff3b2afa3dd45
Praveen Raj <115168832+praveen-raj-143@users.noreply.github.com>
2023-08-07 13:45:43 +0530

    Update Laptops.jsx

--- src/Pages/Laptops.jsx
+++ src/Pages/Laptops.jsx
@@ -24,7 +24,7 @@ const Laptops = () => {
     .then(data=>{ 
       if(data.data.status==="ok"){
         alert("item added to cart")
-        navigate('/cart') 
+        // navigate('/cart')
       }
       else if(data.data.status==="error"){
         alert("failed to add") 
