631305e46e291afe70f3f8a34e8a922c25947d48
Praveen Raj <115168832+praveen-raj-143@users.noreply.github.com>
2023-08-07 13:46:27 +0530

    Update Allproducts.jsx

--- src/Components/Bestseller/Allproducts.jsx
+++ src/Components/Bestseller/Allproducts.jsx
@@ -29,7 +29,7 @@ const Allproducts = () => {
     .then(data=>{ 
       if(data.data.status==="ok"){
         alert("item added to cart")
-        navigate('/cart')
+        // navigate('/cart')
       }
       else if(data.data.status==="error"){
         alert("failed to add") 
