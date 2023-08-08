b4247f280b7c23c71b1f42c3f41e91f5602a1455
Praveen Raj <115168832+praveen-raj-143@users.noreply.github.com>
2023-08-08 12:21:31 +0530

    Update Cart.jsx

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -37,7 +37,7 @@ const Userdetails = () => {
         .then(data=>{ 
           if(data.data.status==="ok"){
             alert("item deleted")
-                        
+             window.location.reload();             
           }
           else if(data.data.status==="error"){
             alert("failed to delete") 
